e509325935aa79307cc30a053d308f90f38308c3
sarim <sarimk80.10p@gmail.com>
2020-01-27 14:16:18 +0500

    Initial Commit

---
+++ memeGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  memeGenerator
+//
+//  Created by Sarim Khan on 27/01/2020.
+//  Copyright © 2020 Sarim Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ memeGeneratorTests/memeGeneratorTests.swift
@@ -0,0 +1,34 @@
+//
+//  memeGeneratorTests.swift
+//  memeGeneratorTests
+//
+//  Created by Sarim Khan on 27/01/2020.
+//  Copyright © 2020 Sarim Khan. All rights reserved.
+//
+
+import XCTest
+@testable import memeGenerator
+
+class memeGeneratorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
