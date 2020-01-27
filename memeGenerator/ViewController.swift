c5ed8e0acc622236367d86e6c556d3749023f97b
sarim <sarimk80.10p@gmail.com>
2020-01-27 15:04:03 +0500

    initial commit

--- memeGenerator/ViewController.swift
+++ memeGenerator/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var count=0
+    var lable:UILabel!
+    
+    @IBOutlet weak var textLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.lable=textLable
+       }
+    
+    @IBAction func incrementButton(_ sender: Any) {
+        
+        self.count+=1
+        self.textLable.text="\(self.count)"
+        
     }
-
-
 }
 
